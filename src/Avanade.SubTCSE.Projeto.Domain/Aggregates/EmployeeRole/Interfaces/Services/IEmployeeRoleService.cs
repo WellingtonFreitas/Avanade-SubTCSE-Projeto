10e0df66c740ccc527fce8db5793912a9e44b965
WellingtonFreitas <wellington.m.defreitas@gmail.com>
2021-06-23 08:59:57 -0300

    criação do employee e validators

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
@@ -1,7 +1,9 @@
-﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
+﻿using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
 {
     public interface IEmployeeRoleService
     {
-        void AddEmployeeRole(Entities.EmployeeRole employeeRole);
+        Task<Entities.EmployeeRole> AddEmployeeRole(Entities.EmployeeRole employeeRole);
     }
 }
--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
@@ -1,8 +1,10 @@
-﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
+﻿using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
 {
     public class EmployeeRoleService : IEmployeeRoleService
     {
-        public void AddEmployeeRole(Entities.EmployeeRole employeeRole)
+        Task<Entities.EmployeeRole> IEmployeeRoleService.AddEmployeeRole(Entities.EmployeeRole employeeRole)
         {
             throw new System.NotImplementedException();
         }
